779edbf3e013bf2341159afbd0464e9326a86135
kajal-2011 <kajalrekha888@gmail.com>
2023-08-17 03:12:25 +0600

    martials art file added

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,13 @@ import Faq from "@/components/Faq";
 import Gallery from "@/components/Gallery";
 import Hero from "@/components/Hero";
 import Intstructor from "@/components/Intstructor";
-import MartialsArt from "@/components/Martialsart";
+
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import MartialsArt from "@/components/MartialsArt";
 
 const HomePage = () => {
   useEffect(() => {
