db07027c6b5ea959dca70ce22b92479b77b5c943
kajal-2011 <kajalrekha888@gmail.com>
2023-08-22 20:30:18 +0600

    contact image added

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,6 +4,7 @@ import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { toast } from "react-toastify";
 import SectionTitle from "./SectionTitle";
+import Image from "next/image";
 
 const Contact = () => {
   const [formData, setFormData] = useState<{
@@ -61,66 +62,87 @@ const Contact = () => {
   return (
     <section id="contact" className="contact wrapper section-padding">
       <SectionTitle title="contact us" />
-      <form
-        ref={formRef}
-        onSubmit={handleSubmit}
-        className="contact-form flex flex-col text-lg gap-10 py-10 mt-10 md:mt-20 max-w-screen-md mx-auto"
-      >
-        <div className="form-control flex flex-col gap-2">
-          <input
-            data-aos="fade-right"
+      <div className="grid grid-cols lg:grid-cols-[45%_auto] gap-10 mt-28">
+        <div className="w-full h-[88%] mt-10 ">
+          <Image
+            src="https://res.cloudinary.com/dh0ado2ir/image/upload/v1692713402/w_1000_hywuln.avif"
+            alt="contact_image"
+            width={500}
+            height={500}
+            priority
+            className="w-full h-full rounded-lg"
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
             data-aos-duration="1000"
-            type="text"
-            name="fullname"
-            placeholder="Full Name"
-            value={formData.fullName}
-            onChange={(e) =>
-              setFormData({ ...formData, fullName: e.target.value })
-            }
-            required
-            className="fullname border border-light/40 focus:border-light/60 bg-transparent py-6 px-10 rounded-xl outline-none duration-300"
           />
         </div>
-        <div className="form-control flex flex-col gap-2">
-          <input
-            data-aos="fade-left"
-            data-aos-duration="1000"
-            type="email"
-            name="email"
-            placeholder="Email Address"
-            value={formData.email}
-            onChange={(e) =>
-              setFormData({ ...formData, email: e.target.value })
-            }
-            required
-            className=" email border border-light/40 focus:border-light/60 bg-transparent  py-6 px-10 rounded-xl outline-none duration-300"
-          />
-        </div>
+        <div>
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+            className="contact-form flex flex-col text-lg gap-10 py-10  mx-auto"
+          >
+            <div className="form-control flex flex-col gap-2">
+              <input
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                type="text"
+                name="fullname"
+                placeholder="Full Name"
+                value={formData.fullName}
+                onChange={(e) =>
+                  setFormData({ ...formData, fullName: e.target.value })
+                }
+                required
+                className="fullname border border-light/40 focus:border-light/60 bg-transparent py-6 px-10 rounded-xl outline-none duration-300"
+              />
+            </div>
+            <div className="form-control flex flex-col gap-2">
+              <input
+                data-aos="fade-left"
+                data-aos-duration="1000"
+                type="email"
+                name="email"
+                placeholder="Email Address"
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+                required
+                className=" email border border-light/40 focus:border-light/60 bg-transparent  py-6 px-10 rounded-xl outline-none duration-300"
+              />
+            </div>
 
-        <div className="form-control flex flex-col gap-2">
-          <textarea
-            data-aos="fade-right"
-            data-aos-duration="1000"
-            placeholder="Message"
-            name="message"
-            required
-            value={formData.message}
-            onChange={(e) =>
-              setFormData({ ...formData, message: e.target.value })
-            }
-            className="message border border-light/40 focus:border-light/60  bg-transparent rounded-xl outline-none py-8 px-12 resize-none duration-300"
-          />
+            <div className="form-control flex flex-col gap-2">
+              <textarea
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                placeholder="Message"
+                name="message"
+                required
+                value={formData.message}
+                onChange={(e) =>
+                  setFormData({ ...formData, message: e.target.value })
+                }
+                className="message border border-light/40 focus:border-light/60  bg-transparent rounded-xl outline-none py-12 px-16 resize-none duration-300"
+              />
+            </div>
+            <div
+              className="submit  rounded-xl   py-5 px-8 text-center overflow-hidden border border-light/40 bg-accant1/60 font-medium hover:bg-accant1/80  duration-300"
+              data-aos="fade-left"
+              data-aos-duration="1000"
+            >
+              <button
+                role="link"
+                type="submit"
+                className="text-light/80 uppercase"
+              >
+                Send
+              </button>
+            </div>
+          </form>
         </div>
-        <div
-          className="submit  rounded-xl   py-5 px-8 text-center overflow-hidden border border-light/40 bg-accant1/60 font-medium hover:bg-accant1/80  duration-300"
-          data-aos="fade-left"
-          data-aos-duration="1000"
-        >
-          <button role="link" type="submit" className="text-light/80 uppercase">
-            Send
-          </button>
-        </div>
-      </form>
+      </div>
     </section>
   );
 };
